67a680a1a391eb3a268017107626e2da8a4f5eff
Zeroxx <franciscopa91@gmail.com>
2017-07-13 14:13:12 +0200

    Added Axios to ajax requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import Axios from 'axios';
+
 const API_KEY = 'a0dbc46dc00e752867d9b78917cb6013';
+
+const FETCH_WEATHER = 'FETCH_WEATER';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},es`;
+
+    const request = axios.get(url);
+
+
+    return{
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
